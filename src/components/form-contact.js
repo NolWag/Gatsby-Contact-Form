7f3ff4b1b4ef8c5cb85e83e0953022bea4f2b449
Nolan Wagner <Nolandubyu@gmail.com>
2020-05-14 15:47:05 -0400

    form updates

--- src/components/form-contact.js
+++ src/components/form-contact.js
@@ -3,40 +3,86 @@ import { Component } from "react"
 import { css } from '@emotion/core';
 
 
-class FormContact extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {value: ''}
+// class FormContact extends React.Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             name: '',
+//             email: '',
+//             message: ''
+//         }
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+//         this.handleChange = this.handleChange.bind(this);
+//         this.handleSubmit = this.handleSubmit.bind(this);
     
-    }
+//     }
 
-    handleChange(e) {
-        this.setState({value: e.target.value})
-    }
+//     handleChange(e) {
+//         const stateName = e.target.name
+//         this.setState({stateName: e.target.value})
+//     }
 
-    handleSubmit(e) {
-        alert("Submitted" + this.state.value)
-        e.preventDefault()
-    }
+//     handleSubmit(e) {
+//         alert("Submitted" + this.state.name)
+//         e.preventDefault()
+//     }
 
 
-render () {
-    return (
-    <form onSubmit={this.handleSubmit}>
-        Name
-        <input value={this.state.value} onChange={this.handleChange} type="text" name="name" required />
-        {/* Email
-        <input type="email" name="reply_to" required />
-        Message:
-        <textarea name="message" required></textarea> */}
-    <input type="submit" value="Submit" />
-    </form>
-    )
+// render () {
+//     return (
+//     <form onSubmit={this.handleSubmit}>
+//         Name
+//         <input value={this.state.name} onChange={this.handleChange} type="text" name="name" required />
+//         Email
+//         <input value={this.state.email} onChange={this.handleChange} type="text" name="email" required />
+//         Message
+//         <textarea value={this.state.message} onChange={this.handleChange} name="message" required></textarea>
+//     <input type="submit" value="Submit" />
+//     </form>
+//     )
+//   }
+// }
+
+function FormContact() {
+  const [state, setState] = React.useState({
+    firstName: "",
+    lastName: ""
+  })
+
+  function handleChange(evt) {
+    const value = evt.target.value;
+    setState({
+      ...state,
+      [evt.target.name]: value
+    });
   }
+
+  return (
+    <form>
+      <label>
+        First name
+        <input
+          type="text"
+          name="firstName"
+          value={state.firstName}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Last name
+        <input
+          type="text"
+          name="lastName"
+          value={state.lastName}
+          onChange={handleChange}
+        />
+      </label>
+      {state.firstName}
+      {state.lastName}
+    </form>
+  );
 }
+
 
 export default FormContact
   
