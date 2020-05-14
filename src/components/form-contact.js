3ae30b65506f1d3eb9511b457e75e266b9051880
Nolan Wagner <Nolandubyu@gmail.com>
2020-05-14 15:03:17 -0400

    form submitting

--- src/components/form-contact.js
+++ src/components/form-contact.js
@@ -6,24 +6,37 @@ import { css } from '@emotion/core';
 class FormContact extends React.Component {
     constructor(props) {
         super(props)
-        
         this.state = {value: ''}
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    
+    }
+
+    handleChange(e) {
+        this.setState({value: e.target.value})
+    }
+
+    handleSubmit(e) {
+        alert("Submitted" + this.state.value)
+        e.preventDefault()
     }
 
 
 render () {
     return (
-    <form>
+    <form onSubmit={this.handleSubmit}>
         Name
-        <input type="text" name="name" required />
-        Email
+        <input value={this.state.value} onChange={this.handleChange} type="text" name="name" required />
+        {/* Email
         <input type="email" name="reply_to" required />
         Message:
-        <textarea name="message" required></textarea>
-    <button type="submit">Send Message</button>
+        <textarea name="message" required></textarea> */}
+    <input type="submit" value="Submit" />
     </form>
     )
   }
 }
-  export default FormContact
+
+export default FormContact
   
